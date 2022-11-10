0c59334ce3d719cf9b34c4755cbb83a5a2117f50
SATYAJEET SINDHIYANI <samxan2014@gmail.com>
2022-11-10 10:53:16 +0530

    Multiple transactions with optional multi-sig

--- src/MultiSigWallet.sol
+++ src/MultiSigWallet.sol
@@ -3,156 +3,124 @@ pragma solidity ^0.8.17;
 
 contract MultiSigWallet{
     uint public immutable numOfConfirmationsRequired;
-    // uint public txIndex;
-    uint immutable TIME_AFTER_TRANSACTION_WILL_GET_CANCELLED = 4 hours;
-
+    uint txIndex;
     struct Transaction{
+        uint transactionIndex;
         address to;
         uint amount;
         uint initiationTime;
         uint confirmationsDone;
         bytes data;
+        bool executed;
     }
-
-    mapping(address=>bool) isOwner;
-    mapping(address=>bool) approvedAccounts;
-    address[] public owners;
-
-    Transaction currentTransction;
-    // TODO: Might want to keep an array of all the transactions as a history
+    
+    mapping(address=>bool) isApprover;
+    mapping(uint=>mapping(address=>bool)) isTransactionConfirmed;
+    // address[] public approvers;
+    address public owner;
+    Transaction [] public transactions;
 
     event MoneyDeposited(address _depositor,uint _amount);
     event TransactionInitiated(address _owner,address _to,uint _amount);
     event TransactionPartiallyApproved(address _owner,uint _amount,uint _confirmationsDone );
     event TransactionCompletelyApproved(uint _amount);
-    event TransactionPartiallyRevoked(address _owner);
-    event TransactionCompletelyRevoked(address ,uint _amount);
+    event TransactionPartiallyRevoked(uint _txIndex,address owner);
+    event TransactionCompletelyRevoked(uint _txIndex,uint _amount);
+    event TransactionCompletelySucceeded(address _owner,uint _amount);
 
-    constructor(uint _numOfConfirmationsRequired,address[] memory _owners){
+    constructor(uint _numOfConfirmationsRequired,address[] memory _approvers,address _owner){
+        owner = _owner;
+        if(_numOfConfirmationsRequired>0){
+            bool isNumberOfConfirmationsRequiredValid =  _numOfConfirmationsRequired>0 && _numOfConfirmationsRequired <= _approvers.length;
+            require(isNumberOfConfirmationsRequiredValid,"Please enter valid number of confirmations required for a transaction");
 
-        require(_owners.length>0,"Owners required");
-
-        bool isNumberOfConfirmationsRequiredValid =  _numOfConfirmationsRequired>0 && _numOfConfirmationsRequired <= _owners.length;
-        require(isNumberOfConfirmationsRequiredValid,"Please enter valid number of confirmations required for a transaction");
-
-        uint len = _owners.length;
-        for(uint i =0;i<len;){
-            address owner = _owners[i];
-            require(owner!=address(0),"Invalid Owner");
-            require(isOwner[owner],"Owner not unique");
-            isOwner[owner]=true;
-            unchecked{
-                ++i;
+            uint len = _approvers.length;
+            for(uint i =0;i<len;){
+                address approver = _approvers[i];
+                require(approver!=address(0),"Invalid Approver");
+                require(isApprover[approver],"Approver not unique");
+                isApprover[approver]=true;
+                unchecked{
+                    ++i;
+                }
             }
-            owners.push(owner);
         }
         numOfConfirmationsRequired = _numOfConfirmationsRequired;
     }
 
+    modifier onlyMultiSig(){
+        require(numOfConfirmationsRequired>0,"Allowed for multisig wallets only");
+        _;
+    }
+    modifier txExists(uint _txIndex){
+        require(transactions[_txIndex].initiationTime>0,"Transaction does not exist");
+    }
     receive() external payable{
         emit MoneyDeposited(msg.sender,msg.value);
     }
 
     function initiateTransaction(address _to,uint _amount,bytes calldata _data) external {
-        cancelTransaction(); // Remove any stale transactions that has not been completed within TIME_AFTER_TRANSACTION_WILL_GET_CANCELLED
-        //TODO : Cancel any other transactions pending
-        bool isTransactionInitiatedByOwner = isOwner[msg.sender];
-        require(isTransactionInitiatedByOwner,"Transaction can only be initaited by owners");
+        
+        uint _txIndex = txIndex;
+        bool isTransactionInitiatedByOwner = owner==msg.sender;
+        require(isTransactionInitiatedByOwner,"Only owner can initaite a transaction");
 
         uint contractBalance = address(this).balance;
-        bool isEnoughContractBalance = contractBalance >= _amount;
-        require(isEnoughContractBalance,"Not Enough Money in your wallet");
+        bool hasEnoughContractBalance = contractBalance >= _amount;
+        require(hasEnoughContractBalance,"Not Enough Money in your wallet");
         
-        // Means there has not been any transaction from this smart contract
-        currentTransction = Transaction(_to,_amount, block.timestamp,1,_data);
-        approvedAccounts[msg.sender] = true;
+        transactions.push(Transaction(_txIndex++,_to,_amount, block.timestamp,1,_data,false));
+        isTransactionConfirmed[_txIndex][msg.sender] = true;
 
-        // Trigger the cancelTransaction after transactionPeriod just once, can be done using chainlink
-        emit TransactionInitiated(msg.sender,_to,_amount);
+        if(numOfConfirmationsRequired==0){ // not a multiSig wallet
+            publishTransaction(_txIndex);
+        }else{
+            emit TransactionInitiated(msg.sender,_to,_amount);
+        }
+        
     }
 
-    function approveTransaction() external {
-        require(isOwner[msg.sender]==true,"Not an owner");
+    function approveTransaction(uint _txIndex) external onlyMultiSig {
+        require(isApprover[msg.sender]==true,"Not an approver");
 
-        bool hasTransactionInitiated = currentTransction.initiationTime >0;
+        bool hasTransactionInitiated = transactions[_txIndex].initiationTime >0;
         require(hasTransactionInitiated,"Please initiate a new transaction");
         
-        // TODO : Might be a good idea to remove it
-        bool hasAlreadyApproved = approvedAccounts[msg.sender];
+        bool hasAlreadyApproved = isTransactionConfirmed[_txIndex][msg.sender];
         require(hasAlreadyApproved,"You have already approved the transaction");
 
-        approvedAccounts[msg.sender]=true;
-        currentTransction.confirmationsDone++;
+        isTransactionConfirmed[_txIndex][msg.sender]=true;
+        transactions[_txIndex].confirmationsDone++;
 
-        emit TransactionPartiallyApproved(msg.sender,currentTransction.amount,currentTransction.confirmationsDone);
-        if(currentTransction.confirmationsDone==numOfConfirmationsRequired){
-            publishTransaction(); 
+        emit TransactionPartiallyApproved(msg.sender,transactions[_txIndex].amount,transactions[_txIndex].confirmationsDone);
+        if(transactions[_txIndex].confirmationsDone==numOfConfirmationsRequired){
+            publishTransaction(_txIndex); 
         }
     }
 
-    function revokeTransaction() external {
+    function revokeTransaction(uint _txIndex) external onlyMultiSig{
 
-        require(isOwner[msg.sender]==true,"Not an owner");
+        require(isApprover[msg.sender]==true,"Not an owner");
 
-        bool hasTransactionInitiated = currentTransction.initiationTime >0;
+        bool hasTransactionInitiated = transactions[_txIndex].initiationTime >0;
         require(hasTransactionInitiated,"No transaction to revoke");
         
-        bool hasAlreadyApproved = approvedAccounts[msg.sender];
+        bool hasAlreadyApproved = isTransactionConfirmed[_txIndex][msg.sender];
         require(hasAlreadyApproved,"You have NOT approved the transaction YET");
 
-        approvedAccounts[msg.sender]=false;
-        currentTransction.confirmationsDone--;
-        emit TransactionPartiallyRevoked(msg.sender);
+        isTransactionConfirmed[_txIndex][msg.sender]=false;
+        transactions[_txIndex].confirmationsDone--;
+        emit TransactionPartiallyRevoked(_txIndex,msg.sender);
 
-        if(currentTransction.confirmationsDone==0){
-            emit TransactionCompletelyRevoked(msg.sender,currentTransction.amount);
-            currentTransction = Transaction(address(0),0,0,0,"");
+        if(transactions[_txIndex].confirmationsDone==0){ // Some modifications might be needed here
+            emit TransactionCompletelyRevoked(_txIndex,transactions[_txIndex].amount);
         }
     }
 
-    function publishTransaction() internal {
-        // TODO : Write publish logic here
-        (bool sent, ) = currentTransction.to.call{value: currentTransction.amount}(currentTransction.data);
+    function publishTransaction(uint _txIndex) internal {
+        (bool sent, ) = transactions[_txIndex].to.call{value: transactions[_txIndex].amount}(transactions[_txIndex].data);
         require(sent, "Failed to send Ether");
         
-        currentTransction = Transaction(address(0),0,0,0,""); // Check whether we can delete an object or not
-        for(uint i =0;i<owners.length;){
-            address owner = owners[i];
-            bool hasOwnerVoted = approvedAccounts[owner];
-            if(hasOwnerVoted){
-                approvedAccounts[owner]=false;
-            }
-            unchecked {
-                ++i;
-            }
-        }
-        emit TransactionCompletelyApproved(currentTransction.amount);
-    }
-    // This function can be called every four hours, so that it cancels all the transactions 
-    // once it has elapsed more than 4 hrs being in the mempool
-    function cancelTransaction() internal {
-        // We will have to make sure that this transaction cancels after the transactionTimePeriod
-        // Also, we will also want to make sure that once the transaction completes then there is a new time 
-        // scheduled for this function call.
-        if(currentTransction.initiationTime >0){
-            bool isDeadlineOver = block.timestamp > currentTransction.initiationTime + TIME_AFTER_TRANSACTION_WILL_GET_CANCELLED; 
-            if(isDeadlineOver){
-                currentTransction = Transaction(address(0),0,0,0,"");
-                for(uint i =0;i<owners.length;){
-                    address owner = owners[i];
-                    bool hasOwnerVoted = approvedAccounts[owner];
-                    if(hasOwnerVoted){
-                        approvedAccounts[owner]=false;
-                    }
-                    unchecked {
-                        ++i;
-                    }
-                }
-            }
-        }
-        
+        emit TransactionCompletelySucceeded(owner,transactions[_txIndex].amount);
     }
 }
-
-
-// TODO: Multiple transactions handle
